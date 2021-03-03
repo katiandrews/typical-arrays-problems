42b54c8f9525cf8414b10246989ea9cda35d9102
katiandrews <andryushina.eka@gmail.com>
2021-03-03 11:52:44 +0200

    feat: add min(), max() and avg()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+
+    let minNumber = array[0];
+
+    for (i = 1; i < array.length; i++) {
+        if (array[i] < minNumber) {
+            minNumber = array[i];
+        }
+    }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+        return 0;
+    }
+
+    let maxNumber = array[0];
+
+    for (i = 1; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+    return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
+
